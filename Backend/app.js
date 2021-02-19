ebab2a4610835277d2eea9b1eb1e22fc9eccdae8
vicky <srivicky2000@gmail.com>
2021-02-19 12:38:18 +0530

    Packzo Backend - app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const app = express();
+require("dotenv").config();
 
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const chalk = require('chalk');
 
 

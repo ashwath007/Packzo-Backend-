61738f1bcb366a6fedb36eaf286aca436f4c0373
ashwath007 <srivicky2000@gmail.com>
2021-02-24 15:24:31 +0530

    Auth

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
++.+require("dotenv").config();
 const { check, validationResult } = require("express-validator");
 var jwt = require("jsonwebtoken");
 var expressJwt = require("express-jwt");
-
+const User = require('../models/user');
 
 exports.adminSignup = (req, res) => {
     console.log(req.body);
+    const user = new User(req.body);
 }

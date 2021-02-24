258a1c78c22cf884eb7a3bb69d2278d1af736787
vicky <srivicky2000@gmail.com>
2021-02-24 20:59:43 +0530

    Controller Error - Updates Routes Category

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -8,10 +8,10 @@ router.param("productId", getProductById);
 
 //Create a product for STORE
 router.post('/admin/:adminId/product/createproduct/:storeId', isAuthenticated, createProduct);
-router.get('/admin/:adminId/product/getallProducts', getallProduct);
+router.get('/admin/product/getallProducts', getallProduct);
 router.put('/admin/:adminId/product/updateProducts/:productId', isAuthenticated, updateProduct);
 router.delete('/admin/:adminId/product/deleteProducts/:productId', isAuthenticated, deleteProduct);
 // router.delete('/admin/product/deleteProducts/:productId', deleteProduct);
-router.get("/admin/:adminId/product/getcategoryProducts", getAllUniqueCategories);
+router.get("/admin/product/getcategoryProducts", getAllUniqueCategories);
 
 module.exports = router;

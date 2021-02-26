05450b241f31ea30c9f1d2f2bc8b2b0b40da87e3
vicky <srivicky2000@gmail.com>
2021-02-26 13:33:38 +0530

    Location work

--- Backend/controllers/location.js
+++ Backend/controllers/location.js
@@ -1,15 +1,8 @@
 const Store = require("../models/store");
 
 exports.getAllLocation = (req, res) => {
-    Store.find({}, (err, allLoc) => {
-        if (err) {
-            return res.status(404).json({
-                error: err
-            })
-        }
-        return res.json({ // Need to work on Location stuff
-            locations: allLoc.locations,
-            // stores: allLoc
-        })
+    //Here need to work on populating Location
+    Store.find().populate('location').exec((err, allLocations) => {
+        console.log(allLocations.location);
     });
 }

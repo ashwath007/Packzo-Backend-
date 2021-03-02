3a20f4262e6da4af91f3ddedabfdef8083f75004
vicky <srivicky2000@gmail.com>
2021-03-02 10:22:52 +0530

    Admin - createAdmin

--- frontEnd/src/core/Menu.js
+++ frontEnd/src/core/Menu.js
@@ -19,59 +19,10 @@ const Menu = ({ history }) => (
         </Link>
       </li>
       <li className="nav-item">
-        <Link
-          style={currentTab(history, "/cart")}
-          className="nav-link"
-          to="/cart"
-        >
-          Cart
+        <Link style={currentTab(history, "/createAdmin")} className="nav-link" to="/createAdmin">
+          Create Admin
         </Link>
       </li>
-      {isAutheticated() && isAutheticated().user.role === 0 && (
-        <li className="nav-item">
-          <Link
-            style={currentTab(history, "/user/dashboard")}
-            className="nav-link"
-            to="/user/dashboard"
-          >
-            U. Dashboard
-          </Link>
-        </li>
-      )}
-      {isAutheticated() && isAutheticated().user.role === 1 && (
-        <li className="nav-item">
-          <Link
-            style={currentTab(history, "/admin/dashboard")}
-            className="nav-link"
-            to="/admin/dashboard"
-          >
-            A. Dashboard
-          </Link>
-        </li>
-      )}
-      {!isAutheticated() && (
-        <Fragment>
-          <li className="nav-item">
-            <Link
-              style={currentTab(history, "/signup")}
-              className="nav-link"
-              to="/signup"
-            >
-              Signup
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              style={currentTab(history, "/signin")}
-              className="nav-link"
-              to="/signin"
-            >
-              Sign In
-            </Link>
-          </li>
-        </Fragment>
-      )}
-      {isAutheticated() && (
         <li className="nav-item">
           <span
             className="nav-link text-warning"
@@ -84,7 +35,7 @@ const Menu = ({ history }) => (
             Signout
           </span>
         </li>
-      )}
+    
     </ul>
   </div>
 );
